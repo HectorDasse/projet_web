d8e0b2033805a72a26e4dc5af0c01d4fb4ff21f2
SOLENNE DIDRON <solenne.didron@viacesi.fr>
2020-04-01 16:22:00 +0200

    Ajout composant AddIntervention

--- projetWeb/src/app/app.module.ts
+++ projetWeb/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddInterventionComponent } from './add-intervention/add-intervention.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddInterventionComponent
   ],
   imports: [
     BrowserModule,
