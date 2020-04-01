b9967eda94ad9a62f740819c48ee8922aaf37e02
Hector <hector.dasse@orange.fr>
2020-04-01 17:20:54 +0200

    début composant table

--- projetWeb/src/app/app.module.ts
+++ projetWeb/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './composent/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
